9718c0ed3cb5d0291c55ec158064f4d418ce5818
Sanskar Gupta <121814254+sanskargupta12@users.noreply.github.com>
2023-03-15 13:51:54 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	int a, b;
+	cout << "Enter first value: ";
+	cin >> a ;
+	
+	cout <<"Enter another value: ";
+	cin >> b ;
+	
+	char operation;
+	cout << "Enter the operation to be performed: ";
+ 	cin >> operation  ;
+ 	
+ 	switch(operation){
+ 		case '+':
+ 			cout << "Addition is: " << a+b;
+ 			break;
+ 		case '-':
+ 			cout << "Substraction is: "<< a-b;
+ 			break;
+ 		case '*':
+ 			cout << "Multiplication is: "<< a*b;
+ 			break;
+ 		case '/':
+ 			cout << "Division is: "<< a/b;
+ 		    break;
+ 		case '%':
+ 			cout << "Modulus is: "<< a%b;
+ 			break;
+ 		case 'i':
+ 			cout << "Increment is: " << ++a << ", " << ++b;
+ 			break;
+ 		case 'd':
+ 			cout << "Decrement is: " << --a << ", " << --b;
+ 			break;
+ 		case 's':
+ 			cout << "Square root is: " << sqrt(a) << ", " << sqrt(b);
+ 			break;
+ 		case 'l':
+ 			cout << "Logorithm is: " << log(a) << ", " << log(b);
+ 			break;
+		default :
+		    cout << "Invalid operation...";
+			break; 			
+	 }
+ 	
+	return 0;
+}
+
